c3e955d69b5bda25ea1a492243dc86dac8dc6b09
Bui Khac Minh Quan <quanbkm@buis-mbp.v822-stk1-trk26.101.usr.spruce.confederationc.on.ca>
2021-12-29 20:49:19 -0500

    modified manifest file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
 import './App.css';
+import { countries, assetTypes } from './countries';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pickedCountry: '',
+      pickedAsset: 0,
+      inputNameValue: '',
+      inputValue: '',
+      selectedAssets: {
+        bank: [],
+        stock: [],
+        crypto: []
+      },
+    }
+  };
+
+  onSelectCountries = (e) => {
+    this.setState({
+      pickedCountry: e.target.value,
+    })
+  }
+
+  onSelectAsset = (e) => {
+    this.setState({
+      pickedAsset: e.target.value,
+    })
+  }
+
+  onSaveAssetValue = () => {
+    if (this.state.pickedAsset === 0) {
+      this.setState({
+        selectedAssets: {
+          ...this.state.selectedAssets,
+          bank: [
+            ...this.state.selectedAssets.bank,
+            {
+              name: this.state.inputNameValue,
+              currency: 'USD',
+              value: this.state.inputValue,
+            }
+          ]
+        }
+      })
+    } else if (this.state.pickedAsset === 1) {
+      this.setState({
+        selectedAssets: {
+          ...this.state.selectedAssets,
+          stock: [
+            ...this.state.selectedAssets.stock,
+            {
+              name: this.state.inputNameValue,
+              currency: 'USD',
+              value: this.state.inputValue,
+            }
+          ]
+        }
+      })
+    } else {
+      this.setState({
+        selectedAssets: {
+          ...this.state.selectedAssets,
+          crypto: [
+            ...this.state.selectedAssets.crypto,
+            {
+              name: this.state.inputNameValue,
+              currency: 'USD',
+              value: this.state.inputValue,
+            }
+          ]
+        }
+      })
+    }
+    this.setState({
+      inputNameValue: '',
+      inputValue: '',
+    })
+  }
+
+  updateNameInputValue = (e) => {
+    this.setState({
+      inputNameValue: e.target.value,
+    })
+  }
+
+  updateInputValue = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h3>
+          Choose your region:
+      </h3>
+        <select onChange={this.onSelectCountries}>
+          {countries.map((country, index) => {
+            return (
+              <option value={country.countryCode}>
+                {country.countryName}
+              </option>
+            )
+          })}
+        </select>
+        {this.state.pickedCountry !== '' && (
+          <>
+            <h3>
+              Choose your type of asset:
+            </h3>
+            <select defaultValue={this.state.pickedAsset} onChange={this.onSelectAsset}>
+              {assetTypes.map((asset, index) => {
+                return (
+                  <option value={asset.id}>
+                    {asset.name}
+                  </option>
+                )
+              })}
+            </select>
+            {this.state.pickedAsset !== '' && (
+              <>
+                <h3>
+                  Bank Name/Platform Name:
+                </h3>
+                <input value={this.state.inputNameValue} onChange={this.updateNameInputValue} />
+                <br />
+                <h3>
+                  Value:
+                </h3>
+                <span>{countries.find((country) => country.countryCode === this.state.pickedCountry).currencyCode}</span>
+                <input value={this.state.inputValue} onChange={this.updateInputValue} />
+                <br />
+                <button onClick={this.onSaveAssetValue}>
+                  Save
+                </button>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
